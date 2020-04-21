5fe402f89b5f6062b2f9a488567bb61f8046c0a6
Dante785412 <60175464+Dante785412@users.noreply.github.com>
2020-04-21 17:07:44 +0200

    Add files via upload

---
+++ news.js
@@ -0,0 +1,32 @@
+const NEWS_API_KEY = '6d99e08921434e1b9c2e043c7991977a';
+
+var request = require('request');
+
+//console.log(process.argv);
+//var category = process.argv[2] || 'business';
+
+//variablen
+var parameter =process.argv[2];
+var category = process.argv.length > 2 ? process.argv[2] : 'business';
+
+
+request('https://newsapi.org/v2/top-headlines?country=de&apiKey=' + NEWS_API_KEY + '&category=' + category, function (error, response, body){
+    
+    if (response.statusCode === 200) {
+        var bodyObj = JSON.parse(body);
+        console.log('Ergebnisse insgesamt: ' + bodyObj.totalResults);
+    }
+
+    for(var i = 0; i < 5 ; i++) { //statt 5 stand <bodyObj.articles.length>
+        console.log((i+1) + ', ' + bodyObj.articles[i].title);
+    }
+
+
+});
+
+
+
+
+
+
+
